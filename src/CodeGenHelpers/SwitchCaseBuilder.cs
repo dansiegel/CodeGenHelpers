4ca1d29e97a4b8a36198d3741f2d6b2480961bb4
Dan Siegel <me@dansiegel.net>
2020-12-03 09:40:06 -0800

    Adding support for switch cases

---
+++ src/CodeGenHelpers/SwitchCaseBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CodeGenHelpers
+{
+    public sealed class SwitchCaseBuilder : IBuilder
+    {
+        private SwitchBuilder _parent { get; }
+        private string _case { get; }
+        private Action<ICodeWriter> _content;
+
+        public SwitchCaseBuilder(SwitchBuilder parent, string @case)
+        {
+            _parent = parent;
+            _case = @case;
+        }
+
+        public SwitchBuilder WithContent(Action<ICodeWriter> contentDelegate)
+        {
+            _content = contentDelegate ?? EmptyBody;
+            return _parent;
+        }
+
+        public SwitchBuilder WithExpression(string returnValue = "null")
+        {
+            _content = w => w.Append($"{_case} => {returnValue}");
+            return _parent;
+        }
+
+        void IBuilder.Write(ref CodeWriter writer)
+        {
+            if (_parent.Expression)
+            {
+                _content?.Invoke(writer);
+            }
+            else
+            {
+                writer.AppendLine($"case {_case}:");
+                writer.IncreaseIndent();
+                _content?.Invoke(writer);
+                writer.AppendLine("break;");
+                writer.DecreaseIndent();
+            }
+        }
+
+        private static void EmptyBody(ICodeWriter writer) { }
+    }
+}
