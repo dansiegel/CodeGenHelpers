f47a8aff6ac177ebf143f9604f040bb743da886a
Dan Siegel <me@dansiegel.net>
2020-12-16 15:21:31 -0800

    allow Pragma warning disable

--- src/CodeGenHelpers/BuilderBase.cs
+++ src/CodeGenHelpers/BuilderBase.cs
@@ -1,10 +1,28 @@
-﻿namespace CodeGenHelpers
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeGenHelpers
 {
     public abstract class BuilderBase : IBuilder
     {
+        protected readonly List<string> _pragmaWarnings = new List<string>();
+
         internal abstract void Write(ref CodeWriter writer);
 
-        void IBuilder.Write(ref CodeWriter writer) =>
+        void IBuilder.Write(ref CodeWriter writer)
+        {
+            var warnings = string.Join(", ", _pragmaWarnings.Distinct());
+            if(_pragmaWarnings.Any())
+            {
+                writer.AppendUnindentedLine($"#pragma warning disable {warnings}");
+            }
+
             Write(ref writer);
+
+            if (_pragmaWarnings.Any())
+            {
+                writer.AppendUnindentedLine($"#pragma warning restore {warnings}");
+            }
+        }
     }
 }
